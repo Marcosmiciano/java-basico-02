53e35742d22c979c7bb6aca5654f07accd3d3edd
marcosmiciano <miciano.marcos@gmail.com>
2020-03-05 18:54:02 -0300

    Primeiro commit

---
+++ src/br/fai/projeto02/Main.java
@@ -0,0 +1,16 @@
+package br.fai.projeto02;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main app = new Main();
+		app.start();
+
+	}
+
+	private void start() {
+		
+		
+	}
+
+}
